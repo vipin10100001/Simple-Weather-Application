05d5ccec31479055224eb843da32805a3385e2f7
N. VIPIN PRASAD <75900544+vipin10100001@users.noreply.github.com>
2023-08-29 12:21:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+API_KEY='37a85c3e57a637a69a81329c644585a6';
+
+const form=document.querySelector('form');
+const search=document.querySelector('#search');
+const weather=document.querySelector('#weather');
+
+const getWeather=async(city)=>{
+    const
+ url = 
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+ 
+    const response=await fetch(url);
+    const data=await response.json();
+   return  showWeather(data);
+}
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+
+form.addEventListener(
+    "submit",
+    function(event){
+        getWeather(search.value)
+        event.preventDefault();
+
+    } ) 
